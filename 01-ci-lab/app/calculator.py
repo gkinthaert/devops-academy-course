561084bff1d96538b532a2056d708c4198222df8
Geert Kinthaert <gkinthaert@gmail.com>
2026-07-14 18:42:32 -0400

    feat: added operations and function calls

--- 01-ci-lab/app/calculator.py
+++ 01-ci-lab/app/calculator.py
@@ -1,3 +1,6 @@
+import json
+
+
 def add(a, b):
     return a + b
 
@@ -10,10 +13,28 @@ def multiply(a, b):
     return a * b
 
 
+def subtract(a, b):
+    return a - b        
+
+
+def power(a, b):
+    return a ** b
+
+
 def lambda_handler(event, context):
-    operation = event.get('operation')
-    a = event.get('a')
-    b = event.get('b')
+    # function URL / API Gateway  
+    if "body" in event:
+        if isinstance(event["body"], str):
+            body = json.loads(event["body"])   
+        else:
+            body = event["body"]
+    # test from AWS console
+    else:
+        body = event
+
+    a = float(body["a"])
+    b = float(body["b"])
+    operation = body["operation"]
 
     if operation == 'add':
         result = add(a, b)
@@ -21,13 +42,17 @@ def lambda_handler(event, context):
         result = divide(a, b)
     elif operation == 'multiply':
         result = multiply(a, b)
+    elif operation == "substract":
+        result = substract(a, b)
+    elif operation == "power":
+        result = power(a, b)
     else:
         return {
-            'statusCode': 400,
-            'body': 'Invalid operation'
+           statusCode": 400,
+            "body": json.dumps({"error": "Unsupported operation"})
         }
 
     return {
-        'statusCode': 200,
-        'body': result
+         "statusCode": 200,
+        "body": json.dumps({"result": result})
     }
